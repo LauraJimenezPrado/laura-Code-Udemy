4db48f29833010568af7d4a13612da5959da8a3d
Laura Jimenez Prado <lauradjimenezp@hotmail.com>
2025-04-13 17:03:08 -0500

    dia dos Udemy

--- Platzi/U_Proyecto _dia_1.py
+++ Platzi/U_Proyecto _dia_1.py
@@ -2,3 +2,5 @@
 print("el nombre de tu cerveza es: '" + input("Dime cual es tu ciudad de origen: ") + " " + input("como describirias a tu mejor amigo en una sola palabra: ")+ "'")"""
 
 print("El nombre de tu cerveza \nes: '" + input("Que ciudad te gustaria visitar: ") + " " + input("Cual es tu color favorito?: ") + "'\n\nFelicitaciones!! \n gracias por participar")
+
+print("este es el ejercicio del dia uno, finalizado")
